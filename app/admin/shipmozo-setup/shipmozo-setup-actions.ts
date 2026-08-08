95fd06386b9576c6c548993719d62b04004a2f07
delambora <de.lambora.official@gmail.com>
2026-08-09 03:00:51 +0530

    Update shipmozo-setup-actions.ts

--- app/admin/shipmozo-setup/shipmozo-setup-actions.ts
+++ app/admin/shipmozo-setup/shipmozo-setup-actions.ts
@@ -22,12 +22,13 @@ export async function setupWarehouse() {
 
   try {
     const result = await createWarehouse({
-      address_title: "De Lambora Bagayam",
-      name: "De Lambora",
-      address_line_one: "No.71, Vaanavil Nagar, Vairamuthu Street",
-      address_line_two: "Bagayam",
-      pin_code: 632002
-    });
+  address_title: "De Lambora Bagayam",
+  name: "De Lambora",
+  phone: 7708920031,
+  address_line_one: "No.71, Vaanavil Nagar, Vairamuthu Street",
+  address_line_two: "Bagayam",
+  pin_code: 632002
+});
     warehouseId = result.warehouse_id;
   } catch (err: any) {
     redirect(`/admin/shipmozo-setup?error=${encodeURIComponent(err.message)}`);
